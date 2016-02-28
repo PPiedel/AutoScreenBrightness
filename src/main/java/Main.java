bd3996f5f25566f7febc72dd67e139a7065266b2
kamex737 <kamex737@gmail.com>
2016-02-28 18:05:20 +0100

    Sth's not working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,44 +1,9 @@
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+
 
 public class Main {
     public static void main(String[] args) {
-        WebCamManager webCamManager = new WebCamManager();
-        BrightnessManager brightnessManager = new BrightnessManager();
-        BufferedImage image;
-
-        boolean running = true;
-        int i=0;
-        while (running){
-            image = webCamManager.getCameraImage();
-
-            /*File outputfile = new File("saved.png");
-            try {
-                ImageIO.write(image, "png", outputfile);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }*/
-
-            int calculatedBrightness =  brightnessManager.calculateLuminance(image);
-            System.out.println("Calculated brightness " + i + " :  "+calculatedBrightness);
-            i++;
-            try {
-                brightnessManager.setBrightness(calculatedBrightness, 0);
-                Thread.sleep(2000);
-            }catch (InterruptedException e){
-                e.printStackTrace();
-            }catch (IOException e){
-                e.printStackTrace();
-            }
-        }
-
-
-
-
-
-
-
+        MainController mainController = new MainController();
+        mainController.startGui();
+        mainController.startWorking();
     }
 }
