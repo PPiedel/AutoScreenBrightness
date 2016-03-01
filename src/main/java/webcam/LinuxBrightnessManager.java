38fe7f15056f21cda9c53cc1a2fad1864aec9e5c
Kamil Bednarz <kamex737@gmail.com>
2016-03-01 21:35:08 +0100

    Temporary ver. of linux support

--- src/main/java/webcam/LinuxBrightnessManager.java
+++ src/main/java/webcam/LinuxBrightnessManager.java
@@ -11,11 +11,15 @@ import java.io.IOException;
 public class LinuxBrightnessManager extends BrightnessManager {
     @Override
     public void setBrightness(int brightness, int delay) throws IOException, BrightnessSettingException {
-
+		String command = createExecCommand(brightness, delay);
+		Process bashProcess = Runtime.getRuntime().exec(command);
     }
 
     @Override
     public String createExecCommand(int brightness, int delay) {
-        return null;
+        String maxBrightness = Runtime.getRuntime().exec("cat /sys/class/backlight/intel_backlight/max_brightness");
+        
+        String calculatedBrightness = new String("echo "+new Integer(maxBrightness)*(brightness/100)+" | sudo tee /sys/class/backlight/intel_backlight/brightness");
+        return calculatedBrightness;
     }
 }
