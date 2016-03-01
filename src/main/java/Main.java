61c3a9ebb15283a7508e7c294af81f3ea5dd6131
kamex737 <kamex737@gmail.com>
2016-03-01 17:40:10 +0100

    Added Nimbus look

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,24 @@
 import org.apache.commons.lang3.SystemUtils;
 
+import javax.swing.*;
+import javax.swing.UIManager.*;
 public class Main {
     public static void main(String[] args) {
         if(SystemUtils.IS_OS_WINDOWS) {
+
+            /** Nimbus look **/
+            try {
+                for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                    if ("Nimbus".equals(info.getName())) {
+                        UIManager.setLookAndFeel(info.getClassName());
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                // If Nimbus is not available, you can set the GUI to another look and feel.
+            }
+
+
             MainController mainController = new MainController();
             mainController.startGui();
         }
