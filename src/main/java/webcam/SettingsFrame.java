9c4c5e69c607e3dcf86572e16452b36e3ee5310c
PPiedel <pawel.piedel@yahoo.pl>
2016-03-02 19:11:40 +0100

    Options frame created.

---
+++ src/main/java/webcam/SettingsFrame.java
@@ -0,0 +1,18 @@
+package webcam;
+
+import javax.swing.*;
+
+/**
+ * Created by Pawel on 2016-03-02.
+ */
+public class SettingsFrame {
+
+    private JFormattedTextField breakTimeTextField;
+    private JFormattedTextField delayTextField;
+    private JLabel delayLabel;
+    private JLabel breakTimeLabel;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
